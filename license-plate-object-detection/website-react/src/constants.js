81ab86d87c57b0ca5ed7455defa560eea42fb009
marcellinus-witarsah <68455552+marcellinus-witarsah@users.noreply.github.com>
2022-06-02 21:48:57 +0700

    Delete license-plate-object-detection/website-react directory

--- license-plate-object-detection/website-react/src/constants.js
+++
@@ -1,8 +0,0 @@
-// for LicensePLateDetection.js
-export const WIDTH = 640;
-export const HEIGHT = 640;
-export const THRESHOLD = 0.5;
-export const MAXFRAMEDISAPPEARED = 7;
-export const MAXFRAMEFORPROCESSING =5;
-
-// 
